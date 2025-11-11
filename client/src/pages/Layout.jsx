d50e29de92624feb521221c8e6288bae5471d7b0
Manpreet Singh <mannrandhawa004@gmail.com>
2025-11-11 15:39:27 +0530

    dashboard created and chnages in ui

--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -1,30 +1,23 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Navbar from '../components/Navbar'
-import {useSelector} from 'react-redux'
-// import Loader from '../components/Loader'
+import { useSelector } from 'react-redux'
+import Loader from '../components/Loader'
 import Login from './Login'
+import Footer from '../components/home/Footer'
 
 const Layout = () => {
 
-  const {user, loading} = useSelector(state => state.auth)
-
-//   if(loading){
-//     return <Loader />
-//   }
 
   return (
     <div>
-      {
-        user ? (
-        <div className='min-h-screen bg-gray-50'>
-          <Navbar />
-          <Outlet />
-        </div>
-      ) 
-      : <Login />
-      }
-      
+      <div className='min-h-screen bg-gray-50'>
+        <Navbar />
+        <Outlet />
+        <Footer/>
+      </div>
+
+
     </div>
   )
 }
