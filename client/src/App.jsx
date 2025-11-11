f6442f9aa929dd158b2ada90cb17d8d510873b20
Manpreet Singh <mannrandhawa004@gmail.com>
2025-11-11 22:53:22 +0530

    changes in ui of dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,30 @@
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
-
-
+import Layout from './pages/Layout'
+import Dashboard from './pages/Dashboard'
+import Login from './pages/Login'
+import { Toaster } from 'react-hot-toast'
+import ResumeBuilder from './pages/ResumeBuilder'
 
 const App = () => {
-
-
-
   return (
     <>
+
+      <Toaster />
       <Routes>
-        <Route path='/' element={<Home />}/>
+        {/* Public route */}
+        <Route path='/' element={<Home />} />
+        <Route path='/auth' element={<Login />} />
+
+        {/* Protected routes */}
+        <Route path='/dashboard' element={<Layout />}>
+          {/* Index route inside Layout */}
+          <Route index element={<Dashboard />} />
+
+          {/* Add more nested routes here if needed */}
+          <Route path='dashboard' element={<Dashboard />} />
+        </Route>
+          <Route path='builder' element={<ResumeBuilder />} />
       </Routes>
     </>
   )
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -81,6 +81,7 @@ const Dashboard = () => {
         <hr className="border-t border-slate-200 my-12" />
         {/* loop for all resumes */}
 
+        {/* for create a resume */}
         {
           showCreateResume && (
             <form onClick={() => setShowCreateResume(false)} className='fixed inset-0 bg-black/70 backdrop-blur bg-opacity-50 z-10 flex items-center justify-center'>
@@ -91,9 +92,10 @@ const Dashboard = () => {
                 <button className='w-full py-2 bg-indigo-600 text-white rounded hover:bg-indigo-700 transition-colors'>Create Resume</button>
                 <XIcon className='absolute top-4 right-4 text-slate-400 hover:text-slate-600 cursor-pointer transition-colors' onClick={() => { setShowCreateResume(false); setTitle('') }} />
               </div>
-            </form>)
+            </form>
+            )
         }
-
+        {/* for upload a resume */}
         {showUploadResume && (
           <form onClick={() => setShowUploadResume(false)} className='fixed inset-0 bg-black/70 backdrop-blur bg-opacity-50 z-10 flex items-center justify-center'>
             <div onClick={e => e.stopPropagation()} className='relative bg-slate-50 border shadow-md rounded-lg w-full max-w-sm p-6'>
@@ -125,6 +127,21 @@ const Dashboard = () => {
           </form>
         )
         }
+
+        {/* for editing a existing resume */}
+
+        {/* {editResumeId && (
+          <form onSubmit={editTitle} onClick={()=> setEditResumeId('')} className='fixed inset-0 bg-black/70 backdrop-blur bg-opacity-50 z-10 flex items-center justify-center'>
+            <div onClick={e => e.stopPropagation()} className='relative bg-slate-50 border shadow-md rounded-lg w-full max-w-sm p-6'>
+              <h2 className='text-xl font-bold mb-4'>Edit Resume Title</h2>
+              <input onChange={(e)=>setTitle(e.target.value)} value={title} type="text" placeholder='Enter resume title' className='w-full px-4 py-2 mb-4 focus:border-green-600 ring-green-600' required/>
+
+              <button className='w-full py-2 bg-green-600 text-white rounded hover:bg-green-700 transition-colors'>Update</button>
+              <XIcon className='absolute top-4 right-4 text-slate-400 hover:text-slate-600 cursor-pointer transition-colors' onClick={()=> {setEditResumeId(''); setTitle('')}}/>
+            </div>
+          </form>
+        )
+        } */}
       </div>
 
     </div>
