fc3423c1a298bf749eb1872bb572f729e9e8204c
Manpreet Singh <mannrandhawa004@gmail.com>
2026-01-02 11:54:27 +0530

    changes in ui

--- client/src/components/home/Banner.jsx
+++ client/src/components/home/Banner.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
 const Banner = () => {
-  return ( 
-    <div className="w-full py-2.5 font-medium text-sm text-indigo-800 text-center bg-gradient-to-r from-[#a9a7f2] to-[#FDFEFF]">
-        <p><span className="px-3 py-1 rounded-lg text-white bg-indigo-600 mr-2">New</span>AI Feature Added</p>
+  return (
+    <div className="w-full py-2.5 font-medium text-sm text-[#EEA712]  text-center bg-gradient-to-r from-[#EEA727] to-[#FDFEFF]">
+      <p><span className="px-3 py-1 rounded-lg text-white bg-[#EEA727]  mr-2">New</span>AI Feature Added</p>
     </div>
   )
 }
