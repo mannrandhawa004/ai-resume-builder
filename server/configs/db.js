1328b016601a3188d272d35e082b2766f7664430
Manpreet Singh <mannrandhawa004@gmail.com>
2026-01-07 18:09:14 +0530

    intergrating atlas account

--- server/configs/db.js
+++ server/configs/db.js
@@ -5,7 +5,6 @@ const connectDB = async () => {
         mongoose.connection.on("connected", ()=>{console.log("Database connected successfully")})
 
         let mongodbURI = process.env.MONGODB_URI;
-        const projectName = 'resume-builder';
 
         if(!mongodbURI){
             throw new Error("MONGODB_URI environment variable not set")
@@ -15,7 +14,7 @@ const connectDB = async () => {
             mongodbURI = mongodbURI.slice(0, -1)
         }
 
-        await mongoose.connect(`${mongodbURI}/${projectName}`)
+        await mongoose.connect(`${mongodbURI}`)
     } catch (error) {
         console.error("Error connecting to MongoDB:", error)
     }
